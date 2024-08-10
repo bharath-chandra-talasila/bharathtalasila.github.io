fe0f9f335d7185c3b6141f3165f40edefed2dbae
bharath-chandra-talasila <talasilabharathchandra@gmail.com>
2024-08-10 07:34:10 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const menuIcon = document.querySelector(".menu-icon");
+  const navbar = document.querySelector(".navbar");
+
+  menuIcon.addEventListener("click", function () {
+    navbar.classList.toggle("active");
+  });
+});
+
+
+
+var typed = new Typed("#element", {
+    strings: ["Student", "Job Seeker", "Fresher", "Student"],
+    typeSpeed: 60,
+  });
+
+
+  
